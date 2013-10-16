93974c16324f24d8dd57d04d4323f8a70345d5ed
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2013-10-16 23:43:07 +0200

    Added license information.

--- libcopy/helpers.py
+++ libcopy/helpers.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2013 Maik Messerschmidt
+
+# This file is part of copy.
+
+# copy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# copy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with copy.  If not, see <http://www.gnu.org/licenses/>.
+
+
 _FILESIZES = [	(1024**4, "T"),
 				(1024**3, "G"),
 				(1024**2, "M"),
