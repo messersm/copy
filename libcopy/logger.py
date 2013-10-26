24df81470381913652a293fbea3ba23fafc062a3
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2013-10-26 08:13:18 +0200

    Added copyright and docstring to logger.py and manager.py

--- libcopy/logger.py
+++ libcopy/logger.py
@@ -1,9 +1,30 @@
+# Copyright (C) 2013 Maik Messerschmidt
+
+# This file is part of copy.
+
+# copy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# copy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with copy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Simple logger module."""
+
 # standard imports
 import sys
 
 from os.path import basename
 
+
 PROG = basename(sys.argv[0])
+
 
 class BaseLogger(object):
 	def __init__(self):
--- libcopy/manager.py
+++ libcopy/manager.py
@@ -1,11 +1,31 @@
-# standard imports
+# Copyright (C) 2013 Maik Messerschmidt
 
+# This file is part of copy.
+
+# copy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# copy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with copy.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Very simple copymanager module."""
+
+# standard imports
 import sys
 
 # local imports
 from .logger import Logger
 
 class CopyManager(object):
+	"""Takes care of letting the workers work (one after another)."""
+	
 	def __init__(self, options):
 		self.options = options
 		
